5122b2facf3d2466f5b48f4683306cd7318a1618
Manuel <manuel_f@manuel.local>
2025-11-06 12:32:39 -0300

    feat: added keyboard input when sending data

--- Assets/Scripts/Data/ScriptableObjects/VirtualKeyboardAcions.cs
+++ Assets/Scripts/Data/ScriptableObjects/VirtualKeyboardAcions.cs
@@ -1,10 +1,34 @@
 using UnityEngine;
+using TMPro;
 
-public class VirtualKeyboardActions : MonoBehaviour
+namespace AIControlMagicVR.Data.ScriptableObjects
 {
-    public void SendTextToChatbot()
+    public class VirtualKeyboardActions : MonoBehaviour
     {
-        "keyboardText";
-        Debug.Log("Text sent to chatbot");
+        private static readonly string Tag = "keyboardText";
+        public void SendTextToChatbot()
+        {
+            GameObject keyboardText = GameObject.FindWithTag(Tag);
+            if (keyboardText != null)
+            {
+                Debug.Log("Found the gameObject: " + keyboardText.name);
+                var tmpInputField = keyboardText.GetComponent<TMP_InputField>();
+                if (tmpInputField != null)
+                {
+                    var text = tmpInputField.text;
+                    Debug.Log("Text entered: " + text);
+                    tmpInputField.text = "";
+                }
+                else
+                {
+                    Debug.LogError("GameObject tagged '" + Tag + "' does not have a TMP_InputField component attached!");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("No GameObject with the tag '" + Tag + "' found in the scene.");
+            }
+            Debug.Log("Text sent to chatbot");
+        }
     }
 }
