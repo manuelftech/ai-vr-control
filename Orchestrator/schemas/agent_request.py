3ddc3ba2ca5c64f919f03772a2635c521b937414
Manuel <manuel_f@manuel.local>
2025-11-29 17:39:34 -0300

    fix: modified order of tasks in prompt

--- Orchestrator/schemas/agent_request.py
+++ Orchestrator/schemas/agent_request.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
 
-# pattern=r"^([a-zA-Z0-9?]{1,200})$",
+
 class AgentRequest(BaseModel):
-    Prompt: str = Field(None, description="Message sent to the chatbot by the user")
+    Prompt: str = Field(None, pattern=r"^([a-zA-Z0-9\?\s]{1,200})$", description="Message sent to the chatbot by the user")
