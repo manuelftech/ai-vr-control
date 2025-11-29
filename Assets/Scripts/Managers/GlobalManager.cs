93fb5c7fb60d6dad7b935959fea4145481681599
Manuel <manuel_f@manuel.local>
2025-11-29 12:15:38 -0300

    fix: modified reference to local element

--- Assets/Scripts/Managers/GlobalManager.cs
+++ Assets/Scripts/Managers/GlobalManager.cs
@@ -154,7 +154,7 @@ namespace AIControlVR.Managers
                 }
                 Renderer renderer = sceneVRState.Value.GetComponent<Renderer>();
                 ConstantForce constantForce = sceneVRState.Value.GetComponent<ConstantForce>();
-                TextMeshProUGUI tmpInputField = localGameObject.GetComponent<TextMeshProUGUI>();
+                TextMeshProUGUI tmpInputField = sceneVRState.GetComponent<TextMeshProUGUI>();
                 foreach (VRProperty vr in updatedVRStates.Properties){
                     switch(vr.Name){
                         case "$.Components.Color":
